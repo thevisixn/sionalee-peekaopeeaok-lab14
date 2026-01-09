390db0ccaed9aa4c351c2e34c543f9eb18f63e57
visionalee <flook.xtnt@gmail.com>
2026-01-09 16:30:54 +0700

    (2)

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -20,4 +20,36 @@ int main(){
 	return 0;
 }
 
-//Do not modify source code above this line 
+//Do not modify source code above this line
+void showMatrix(const bool bdata[][N]){
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++){
+			cout << bdata[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
+void inputMatrix(double data[][N]){
+	for(int i = 0; i < N; i++){
+		cout << "Row " << i+1 << ": ";
+		for(int j = 0; j < N; j++){
+			cin >> data[i][j];
+		}
+	}
+}
+
+void findLocalMax(const double data[][N], bool bData[][N]){
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++){
+			bData[i][j] = false;
+		}
+	}
+
+	for(int i = 1; i < N-1; i++){
+		for(int j = 1; j < N-1; j++){
+			if(data[i][j] >= data[i-1][j] && data[i][j] >= data[i+1][j] && data[i][j] >= data[i][j-1] && data[i][j] >= data[i][j+1]){
+				bData[i][j] = true;
+			}
+		}
+	}
+}
